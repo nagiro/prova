6190f20c219fe8209e88454c64b3eb0fdd1abf66
Albert Johé <albert.johe@gmail.com>
2010-01-14 16:59:32 +0000

    Intranet: Modificació gestio_apps
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@175 6722b150-0e3f-45da-bd85-83856719fb75

--- apps/intranet/templates/gestio_apps.php
+++ apps/intranet/templates/gestio_apps.php
@@ -25,11 +25,11 @@
       <TD class="MENU">
         <CENTER>
           <TABLE class="MENU_TABLE">
-            <TR><TD class="SUBMENU1"><?php echo imgSub1().' DOCUMENTS'; ?></TD></TR>              
-              <TR><TD class="SUBMENU2"><?php echo link_to(imgSub2().' Torna directori base','apps/gDocuments?accio=CD&IDD=1'); ?></TD></TR>
-			<TR><TD class="SUBMENU1"><?php echo imgSub1().' APLICACIONS'; ?></TD></TR>
-			  <TR><TD class="SUBMENU2"><?php echo link_to(imgSub2().' Surt','web/logout'); ?></TD></TR>
-			  <TR><TD class="SUBMENU2"><?php echo link_to(imgSub2().' Torna a la web','web/index'); ?></TD></TR>			
+            <TR><TD class="SUBMENU_1"><?php echo imgSub1().' DOCUMENTS'; ?></TD></TR>              
+              <TR><TD class="SUBMENU_2"><?php echo link_to(imgSub2().' Torna directori base','apps/gDocuments?accio=CD&IDD=1'); ?></TD></TR>
+			<TR><TD class="SUBMENU_1"><?php echo imgSub1().' APLICACIONS'; ?></TD></TR>
+			  <TR><TD class="SUBMENU_2"><?php echo link_to(imgSub2().' Surt','web/logout'); ?></TD></TR>
+			  <TR><TD class="SUBMENU_2"><?php echo link_to(imgSub2().' Torna a la web','web/index'); ?></TD></TR>			
           </TABLE>
         </CENTER>
         </TD>      
