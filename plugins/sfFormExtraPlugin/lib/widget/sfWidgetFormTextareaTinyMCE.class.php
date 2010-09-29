d8607832591952464b2d290059bedb922416ebc0
Albert Johé <albert.johe@gmail.com>
2010-09-29 08:53:54 +0000

    Intranet: Possibilitat d'enganxar un text fet en word.
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@431 6722b150-0e3f-45da-bd85-83856719fb75

--- plugins/sfFormExtraPlugin/lib/widget/sfWidgetFormTextareaTinyMCE.class.php
+++ plugins/sfFormExtraPlugin/lib/widget/sfWidgetFormTextareaTinyMCE.class.php
@@ -62,11 +62,12 @@ class sfWidgetFormTextareaTinyMCE extends sfWidgetFormTextarea
   tinyMCE.init({
     mode:                              "exact",
     elements:                          "%s",
-    plugins:                           "media,table",        
+    plugins:                           "media,table,paste",        
     theme:                             "%s",
     language:                          "ca",
     %s
     %s
+    paste_strip_class_attributes:      "none",
     theme_advanced_toolbar_location:   "top",
     theme_advanced_toolbar_align:      "left",
     theme_advanced_statusbar_location: "bottom",
@@ -76,7 +77,7 @@ class sfWidgetFormTextareaTinyMCE extends sfWidgetFormTextarea
 	theme_advanced_buttons2 :          "",
 	theme_advanced_buttons3 :          "",
 	file_browser_callback:             'ajaxfilemanager',
-	relative_urls:  				   false,	
+	relative_urls:  				   false,	    
     %s
   })
   function ajaxfilemanager(field_name, url, type, win) {
