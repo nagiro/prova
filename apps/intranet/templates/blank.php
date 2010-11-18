4901ac7cf2a4a8925812fce00723bf00637631bb
Albert Johé <albert.johe@gmail.com>
2010-11-18 10:54:16 +0000

    Intranet: Hospici 0.9
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@522 6722b150-0e3f-45da-bd85-83856719fb75

--- apps/intranet/templates/blank.php
+++ apps/intranet/templates/blank.php
@@ -1,3 +1,26 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html>  <head>  <meta http-equiv="content-type" content="text/html; charset=utf-8">  <meta name="generator" content="PSPad editor, www.pspad.com">    <?php include_http_metas() ?>  <?php include_metas() ?>    <?php include_title() ?>
-  <?php include_stylesheets() ?>
-  <?php include_javascripts() ?>    <title></title>      </head>  <body>    <?php echo $sf_content ?>  </body></html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+  
+  <?php $BASE = OptionsPeer::getString('SF_WEBROOT',1); ?>
+
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $BASE.'css/gestio.css'; ?>" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $BASE.'css/smoothness/jquery-ui-1.7.2.custom.css'; ?>" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $BASE.'css/jquery-datepick/jquery.datepick.css'; ?>" />      
+
+  <script type="text/javascript" src="<?php echo $BASE.'js/jquery-1.4.2.min.js'; ?>"></script>
+  <script type="text/javascript" src="<?php echo $BASE.'js/tiny_mce/tiny_mce.js'; ?>"></script>              
+  <script type="text/javascript" src="<?php echo $BASE.'js/jquery-ui/js/jquery-ui-1.7.2.custom.min.js'; ?>"></script>
+  <script type="text/javascript" src="<?php echo $BASE.'js/jquery.datepick.package-3.7.1/jquery.datepick.js'; ?>"></script>
+  <script type="text/javascript" src="<?php echo $BASE.'js/jquery.datepick.package-3.7.1/jquery.datepick-ca.js'; ?>"></script>
+  <script type="text/javascript" src="<?php echo $BASE.'js/jquery.cookie.js'; ?>"></script>          
+    
+  <title></title>    
+  </head>
+  <body>
+    <?php echo $sf_content ?>
+  </body>
+</html>
