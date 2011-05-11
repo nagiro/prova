be6ab552178a82b637abbf95c0392a5f1f19fa48
Albert Johé <albert.johe@gmail.com>
2011-05-11 11:32:27 +0000

    Intranet: Correccions en l'addició d'espais a la intranet. Canvi dels formularis per un model més simplista.
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@650 6722b150-0e3f-45da-bd85-83856719fb75

--- apps/intranet/modules/web/templates/_espais.php
+++ apps/intranet/modules/web/templates/_espais.php
@@ -14,12 +14,12 @@
 	
 	</style>
 
-    <TD colspan="3" id="TEXT_WEB" class="CONTINGUT">
+    <td colspan="3" id="TEXT_WEB" class="CONTINGUT">
 
 	<div style="padding-right:40px; text-align:left;">
 
 		<h2> Espais de la Casa de Cultura de Girona</h2>	
-		<p> La Fundació Casa de Cultura de Girona, a més de la seva oferta d’activitats, ofereix un servei de cessió d’espais a persones i entitats que ho requereixin. Per demanar la utilització dels espais del Centre podeu consultar la disponibilitat de sales i equips així com les condicions de cessió a la Secretaria o en aquesta mateixa pàgina web. Cal formalitzar la sol•licitud omplint un formulari que es pot presentar a la Casa de Cultura (Plaça Hospital, 6. Girona) o tramitar en línia. Podeu accedir al formulari clicant als enllaços que trobareu a la descripció de cadascun dels espais o sol•licitar-lo a la mateixa Casa de Cultura. </p>
+		<p> La Fundació Casa de Cultura de Girona, a més de la seva oferta d’activitats, ofereix un servei de cessió d’espais a persones i entitats que ho requereixin. Per demanar la utilització dels espais del Centre podeu consultar la disponibilitat de sales i equips així com les condicions de cessió a la Secretaria o en aquesta mateixa pàgina web. Cal formalitzar la sol·licitud omplint un formulari que es pot presentar a la Casa de Cultura (Plaça Hospital, 6. Girona) o tramitar en línia. Podeu accedir al formulari clicant als enllaços que trobareu a la descripció de cadascun dels espais o sol·licitar-lo a la mateixa Casa de Cultura. </p>
 	    
 	    <br />
 		<br />
@@ -44,7 +44,7 @@
             endforeach;
         
         ?>
-         
+<!--         
 	    <h3>Auditori Josep Viader</h3>
 	    <div class="col1">Situació: </div>		<div class="col2">Planta baixa</div>    
 	    <div class="col1">Aforament: </div>		<div class="col2">110 persones</div>
@@ -89,7 +89,7 @@
 		<div class="col1">Fotografies: </div>	<div class="col2"><?php echo generaImatge('Aula1.jpg','Aula 1. Vista general'); ?><?php echo generaImatge('Aula2.jpg','Aula 2. Vista general'); ?><?php echo generaImatge('Aula3.jpg','Aula 3. Vista general'); ?><?php echo generaImatge('Aula4.jpg','Aula 4. Vista general'); ?><?php echo generaImatge('Aula5.jpg','Aula 5. Vista general'); ?><?php echo generaImatge('Aula6.jpg','Aula 6. Vista general'); ?><?php echo generaImatge('Aula7.jpg','Aula 7. Vista general'); ?></div>
 		<div class="col3"><?php echo link_to("Sol·licitar cessió d'espais",'gestio/uGestio?accio=GESTIONA_RESERVES') ?> </div>	    
 		<div class="fi">&nbsp;</div>
-	  	 
+-->	  	 
         <a name="condicions"></a>	
 		<h3>Condicions de cessió i ús</h3>
 		<p>
@@ -98,18 +98,18 @@
 <li>1. La cessió d’espais es factura segons els trams indicats en les tarifes. En el cas que una cessió superi la durada prevista d’ocupació de l’espai, cada hora de més es facturarà a 30 euros o s’aplicarà el tram de tarifa immediatament superior, tant en ús comercial com en ús cultural.  En tot cas, sempre s’aplicarà la mesura que sigui més avantatjosa per a l’usuari.
 </li><li>2. A partir de les 21 hores, cada hora o fragment d’hora que superi el temps de cessió de l’espai tindrà un cost de 30 euros (40 en cap de setmana), tant en tarifa cultural com en tarifa comercial. 
 </li><li>3. La cessió d’espai significa una disponibilitat de sales i de material però no pressuposa l’assistència permanent dels tècnics de la Casa de Cultura a l’acte programat. Aquesta assistència anirà a  càrrec de l’organitzador de l’acte.
-</li><li>4. En cas de requerir assistència tècnica continuada, el sol•licitant haurà de contractar serveis professionals. La Casa de Cultura pot facilitar-li el contacte amb els seus proveïdors habituals
+</li><li>4. En cas de requerir assistència tècnica continuada, el sol·licitant haurà de contractar serveis professionals. La Casa de Cultura pot facilitar-li el contacte amb els seus proveïdors habituals
 </li><li>5. En cap de setmana o festius, la cessió d’espais té un preu de partida de 150 euros en concepte d’assistència i obertura del centre fora dels dies i horaris habituals d’activitat per a mitja jornada i de 250 euros per a jornada completa. Aquesta clàusula s’aplica tant en tarifa comercial com cultural. El preu de lloguer de l’espai se sumarà a les quantitats indicades anteriorment.
 </li><li>6. La cessió d’un espai no obliga la Casa de Cultura a incloure l’activitat acollida en el material de difusió, ni imprès ni electrònic, de la seva programació de la Casa de Cultura. 
-</li><li>7. No es consideraran, en principi, les sol•licituds que tinguin una previsió superior als 90 dies.
-</li><li>8. La Casa de Cultura es reserva el dret de suspendre una sol•licitud aprovada quan concorrin raons de força major o per coincidència amb activitat pròpia de la Fundació Casa de Cultura. En aquest cas proposarà una alternativa als organitzadors de l’acte suspès amb la màxima antelació possible.
-</li><li>9. Els desperfectes per ús indegut dels aparells o instal•lacions aniran càrrec del sol•licitant.
+</li><li>7. No es consideraran, en principi, les sol·licituds que tinguin una previsió superior als 90 dies.
+</li><li>8. La Casa de Cultura es reserva el dret de suspendre una sol·licitud aprovada quan concorrin raons de força major o per coincidència amb activitat pròpia de la Fundació Casa de Cultura. En aquest cas proposarà una alternativa als organitzadors de l’acte suspès amb la màxima antelació possible.
+</li><li>9. Els desperfectes per ús indegut dels aparells o instal·lacions aniran càrrec del sol·licitant.
 </li><li>10. La Casa de Cultura de Girona es reserva el dret de denegar la cessió d’un espai a qualsevol activitat que contradigui els seus objectius fundacionals o no s’ajusti a la seva programació cultural.
-</li><li>11. Els serveis de cessió inclouen el de neteja i seguretat bàsics, i el de la responsabilitat civil. Segons la naturalesa de l'activitat proposada, la Casa de Cultura de Girona pot demanar al sol•licitant que es faci càrrec de serveis extraordinaris (assegurança, serveis mèdics, serveis tècnics professionals per a la manipulació d’equips audiovisuals, etc.)
+</li><li>11. Els serveis de cessió inclouen el de neteja i seguretat bàsics, i el de la responsabilitat civil. Segons la naturalesa de l'activitat proposada, la Casa de Cultura de Girona pot demanar al sol·licitant que es faci càrrec de serveis extraordinaris (assegurança, serveis mèdics, serveis tècnics professionals per a la manipulació d’equips audiovisuals, etc.)
 </li><li>12. La venda de productes o serveis necessitarà d'autorització expressa de la Casa de Cultura.
-</li><li>13. El sol•licitant en nom propi o dels organitzadors (si és el cas) garanteix que l’activitat programada respectarà el compliment dels drets i deures en matèria de propietat intel•lectual en relació a les obres, interpretacions o activitats que es portin a terme, i assumirà les despeses que se’n puguin derivar.
+</li><li>13. El sol·licitant en nom propi o dels organitzadors (si és el cas) garanteix que l’activitat programada respectarà el compliment dels drets i deures en matèria de propietat intel·lectual en relació a les obres, interpretacions o activitats que es portin a terme, i assumirà les despeses que se’n puguin derivar.
 </li><li>14. La Casa de Cultura de Girona no es fa responsable de rètols o altres elements publicitaris enganxats o dipositats en espais no autoritzats.
-</li><li>15. Les dades personals que el sol•licitant faciliti quedaran enregistrades en un fitxer titularitat de la Fundació Casa de Cultura creat per a gestionar les activitats que s’hi porten a terme. La Casa de Cultura es compromet a complir els seus deures de mantenir reserva i d’adoptar les mesures legalment previstes i tècnicament necessàries per evitar-ne un accés o qualsevol classe de tractament no autoritzat. En qualsevol cas el titular de les dades pot exercir els seus drets d’accés, rectificació i cancel•lació tot adreçant-se a: Sr/a. Director/a de la Casa de Cultura, Plaça de l’Hospital 6, 17002 GIRONA, telèfon al 972 202 013 i correu electrònic  secretaria@casadecultura.org.
+</li><li>15. Les dades personals que el sol·licitant faciliti quedaran enregistrades en un fitxer titularitat de la Fundació Casa de Cultura creat per a gestionar les activitats que s’hi porten a terme. La Casa de Cultura es compromet a complir els seus deures de mantenir reserva i d’adoptar les mesures legalment previstes i tècnicament necessàries per evitar-ne un accés o qualsevol classe de tractament no autoritzat. En qualsevol cas el titular de les dades pot exercir els seus drets d’accés, rectificació i cancel·lació tot adreçant-se a: Sr/a. Director/a de la Casa de Cultura, Plaça de l’Hospital 6, 17002 GIRONA, telèfon al 972 202 013 i correu electrònic  secretaria@casadecultura.org.
 </li>
                         
             
@@ -121,76 +121,62 @@
         <p>En horari de dilluns a divendres, de 9 a 21 hores</p>
 		<p>
             <table>                
-                <tr><td colspan="3">TARIFA CULTURAL</td></tr>
-                <tr><td></td><td>Fins a tres hores</td><td>Mitja jornada</td></tr>
-                <tr><td>Auditori Josep Viader</td>  <td>100€</td><td>150€</td></tr>
-                <tr><td>Aula Magna</td>             <td> 75€</td><td>125€</td></tr>
-                <tr><td>Aula B</td>                 <td> 60€</td><td> 90€</td></tr>
-                <tr><td>Aula C</td>                 <td> 40€</td><td> 60€</td></tr>
-                <tr><td>Aules 2n pis</td>           <td> 30€</td><td> 45€</td></tr>
-                <tr><td>Sala Domènech Fita</td>     <td> 60€</td><td> 90€</td></tr>
-                <tr><td>Pati</td>                   <td> 75€</td><td>125€</td></tr>                
-                <tr><td>&nbsp;</td>                 <td></td><td></td></tr>
-                <tr><td></td><td>Jornada completa</td><td></td></tr>
-                <tr><td>Auditori Josep Viader</td>  <td>225€</td><td></td></tr>
-                <tr><td>Aula Magna</td>             <td>175€</td><td></td></tr>
-                <tr><td>Aula B</td>                 <td>125€</td><td></td></tr>
-                <tr><td>Aula C</td>                 <td>85€</td><td></td></tr>
-                <tr><td>Aules 2n pis</td>           <td>65€</td><td></td></tr>
-                <tr><td>Sala Domènech Fita</td>     <td>125€</td><td></td></tr>
-                <tr><td>Pati</td>                   <td>175€</td><td></td></tr>
-
-                <tr><td colspan="3">TARIFA COMERCIAL</td></tr>
-                <tr><td></td><td>Fins a tres hores</td><td>Mitja jornada</td></tr>
-                <tr><td>Auditori Josep Viader</td>  <td>200€</td><td>300€</td></tr>
-                <tr><td>Aula Magna</td>             <td>150€</td><td>225€</td></tr>
-                <tr><td>Aula B</td>                 <td>100€</td><td>150€</td></tr>
-                <tr><td>Aula C</td>                 <td> 75€</td><td>110€</td></tr>
-                <tr><td>Aules 2n pis</td>           <td> 50€</td><td> 75€</td></tr>
-                <tr><td>Sala Domènech Fita</td>     <td>100€</td><td>150€</td></tr>
-                <tr><td>Pati</td>                   <td>120€</td><td>180€</td></tr>                
-                <tr><td>&nbsp;</td>                 <td></td><td></td></tr>
-                <tr><td></td><td>Jornada completa</td><td></td></tr>
-                <tr><td>Auditori Josep Viader</td>  <td>450€</td><td></td></tr>
-                <tr><td>Aula Magna</td>             <td>325€</td><td></td></tr>
-                <tr><td>Aula B</td>                 <td>200€</td><td></td></tr>
-                <tr><td>Aula C</td>                 <td>150€</td><td></td></tr>
-                <tr><td>Aules 2n pis</td>           <td>100€</td><td></td></tr>
-                <tr><td>Sala Domènech Fita</td>     <td>200€</td><td></td></tr>
-                <tr><td>Pati</td>                   <td>325€</td><td></td></tr>
-
-                
-                <tr><td>APARELLS AUDIOVISUALS.</td><td>TARIFA CULTURAL</td><td>TARIFA COMERCIAL</td></tr>                
+                <tr><th colspan="3">TARIFA CULTURAL</th></tr>
+                <tr><td></td><td>Fins a tres hores</td><td>Mitja jornada</td><td>Jornada completa</td></tr>
+                <tr><td>Auditori Josep Viader</td>  <td>100€</td><td>150€</td><td>225€</td></tr>
+                <tr><td>Aula Magna</td>             <td> 75€</td><td>125€</td><td>175€</td></tr>
+                <tr><td>Aula B</td>                 <td> 60€</td><td> 90€</td><td>125€</td></tr>
+                <tr><td>Aula C</td>                 <td> 40€</td><td> 60€</td><td>85€</td></tr>
+                <tr><td>Aules 2n pis</td>           <td> 30€</td><td> 45€</td><td>65€</td></tr>
+                <tr><td>Sala Domènech Fita</td>     <td> 60€</td><td> 90€</td><td>125€</td></tr>
+                <tr><td>Pati</td>                   <td> 75€</td><td>125€</td><td>175€</td></tr>                                                
+            </table>
+            <br />
+            <table>
+                <tr><th colspan="3">TARIFA COMERCIAL</th></tr>
+                <tr><td></td><td>Fins a tres hores</td><td>Mitja jornada</td><td>Jornada completa</td></tr>
+                <tr><td>Auditori Josep Viader</td>  <td>200€</td><td>300€</td><td>450€</td></tr>
+                <tr><td>Aula Magna</td>             <td>150€</td><td>225€</td><td>325€</td></tr>
+                <tr><td>Aula B</td>                 <td>100€</td><td>150€</td><td>200€</td></tr>
+                <tr><td>Aula C</td>                 <td> 75€</td><td>110€</td><td>150€</td></tr>
+                <tr><td>Aules 2n pis</td>           <td> 50€</td><td> 75€</td><td>100€</td></tr>
+                <tr><td>Sala Domènech Fita</td>     <td>100€</td><td>150€</td><td>200€</td></tr>
+                <tr><td>Pati</td>                   <td>120€</td><td>180€</td><td>325€</td></tr>
+            </table>
+            <br />
+            <table>
+                <tr><th>APARELLS AUDIOVISUALS.</th><th>TARIFA CULTURAL</th><th>TARIFA COMERCIAL</th></tr>                
                 <tr><td>Projector </td>                 <td>30€</td><td>60€</td></tr>
                 <tr><td>Aparell reproductor</td>        <td>15€</td><td>30€</td></tr>
                 <tr><td>Piano Auditori</td>             <td>50€</td><td>100€</td></tr>
                 <tr><td>Piano Aula Magna</td>           <td>35€</td><td>70€</td></tr>
                 <tr><td>Pantalla de retroprojecció</td> <td>30€</td><td>60€</td></tr>
-                <tr><td colspan="3">Si l’ús dels pianos requereix un afinació no programada, el cost anirà a càrrec dels usuaris.</td></tr>                                
-                                
+                <tr><td colspan="3">Si l’ús dels pianos requereix un afinació no programada, el cost anirà a càrrec dels usuaris.</td></tr>                                                                
             </table>  
 	    </p>
-
+        
+        <br />
+        <br />
         <a name="criteris"></a>	
 		<h3>Criteris</h3>
-        
+        <br />
         <h4>Aplicació de tarifa comercial</h4>
-            <p>En principi, es considerarà activitat comercial tota aquella generada per una entitat privada amb afany de lucre. És possible, tanmateix, que les entitats o empreses privades sol•licitin espais o serveis per a una activitat d’interès general. En aquest cas, és possible autoritzar l’activitat amb exempció de despeses. La Casa de Cultura es reserva el dret d’autoritzar o denegar aquesta exempció.</p> 
+            <p>En principi, es considerarà activitat comercial tota aquella generada per una entitat privada amb afany de lucre. És possible, tanmateix, que les entitats o empreses privades sol·licitin espais o serveis per a una activitat d’interès general. En aquest cas, és possible autoritzar l’activitat amb exempció de despeses. La Casa de Cultura es reserva el dret d’autoritzar o denegar aquesta exempció.</p> 
             <p>Es consideraran activitats comercials també aquelles que, provenint de l’àmbit públic o privat, no s’adrecin a la ciutadania en general i per a les quals calgui una matrícula, és a dir, que generin un ingressos. És el cas de jornades, congressos, cursos...</p>
- 
+        <br /><br />
         <h4>Aplicació de tarifa cultural</h4>
             <p>S’aplica la tarifa cultural a aquelles cessions d’espai a entitats i activitats d’interès social, cultural i/o sense afany de lucre.</p> 
             <p>Aquestes activitats poden demanar l’exempció de despeses, que la Fundació Casa de Cultura aprovarà o desestimarà d’acord amb els seus criteris de programació i objectius fundacionals.</p>
-
+        <br /><br />
         <h4>Diputació de Girona i organismes vinculats</h4>
-            <p>Tots els organismes dependents –directament o indirecta- de la Diputació de Girona estan exempts del pagament de despeses però han de sol•licitar els espais o serveis de manera formalitzada.</p>            
+            <p>Tots els organismes dependents –directament o indirecta- de la Diputació de Girona estan exempts del pagament de despeses però han de sol·licitar els espais o serveis de manera formalitzada.</p>            
             <p>En cap cas, la dependència o vinculació amb la Diputació de Girona suposa un tracte preferencial en relació als compromisos ja adquirits o en relació a la pròpia activitat de la Fundació.</p>
    
    </div>
    
-      <DIV STYLE="height:80px;"></DIV>
+      <div style="height:80px;"></div>
                 
-    </TD>
+    </td>
     
     
    <?php 
