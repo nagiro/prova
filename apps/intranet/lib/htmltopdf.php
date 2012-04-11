502047d671305475495f4630fa414e441d9d2268
Albert Johé <albert.johe@gmail.com>
2012-04-11 11:50:24 +0000

    BUG: wkhtmltopdf QPixmap: Cannot create a QPixmap when no GUI is being used
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@819 6722b150-0e3f-45da-bd85-83856719fb75

--- apps/intranet/lib/htmltopdf.php
+++ apps/intranet/lib/htmltopdf.php
@@ -43,14 +43,7 @@ class HTML2PDF{
             // Note: On a live site you should probably log the error and give a
             // more generic error message, for security
             echo 'PDF GENERATOR ERROR:<br />' . nl2br(htmlspecialchars($errors));
-            header('Content-Type: application/pdf');
-            header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
-            header('Pragma: public');
-            header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-            header('Last-Modified: ' . gmdate('D, d M Y H:i:s').' GMT');
-            header('Content-Length: ' . strlen($pdf));
-            header('Content-Disposition: inline; filename="' . $filename . '";');
-            echo $pdf;            
+            
         } else {
             header('Content-Type: application/pdf');
             header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
