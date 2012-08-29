9e3046499fa5560927b6fd1c04a5005c33168553
Albert Johé <albert.johe@gmail.com>
2012-08-29 11:00:42 +0000

    BUG: Les variables de correu, site, telèfon i data d'inici de curs no apareixien correctament al detall del curs un cop registrat.
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@842 6722b150-0e3f-45da-bd85-83856719fb75

--- apps/hospici/modules/web/templates/_showDetallCurs.php
+++ apps/hospici/modules/web/templates/_showDetallCurs.php
@@ -54,6 +54,8 @@
             $url        = url_for('@hospici_detall_curs?idC='.$CURS->getIdcursos().'&titol='.$CURS->getNomForUrl());
             $ESTAT      = myUser::ph_EstatCurs($AUTH, $CURS, $url, $CURSOS_MATRICULATS);                         
             $OS         = SitesPeer::retrieveByPK($CURS->getSiteId());
+            $nom        = $OS->getNom(); $email  = $OS->getEmailString(); $tel    = $OS->getTelefonString();
+            
             
                         
             //Carrego la imatge del site
@@ -115,7 +117,7 @@
                                             case 'NO_HI_PLACES'     : echo '<div>Aquest curs ja no té places lliures.<br /><br /> Si vol pot matricular-s\'hi igualment i restarà en llista d\'espera. En el cas que s\'alliberi alguna plaça, que vostè pot ocupar, el trucarem el més aviat possible. Per a més informació, pot posar-se en contacte amb <b>'.$nom.'</b> enviant un correu electrònic a <b>'.$email.'</b> o bé trucant al telèfon <b>'.$tel.'</b>.<br /></div>'; break;                                            
                                             case 'NO_HI_HA_RESERVA_LINIA': echo '<div>Aquest curs no disposa de matrícula en línia.<br /><br /> Per poder-s\'hi matricular, ha de posar-se en contacte amb <b>'.$nom.'</b> enviant un correu electrònic a <b>'.$email.'</b> o bé trucant al telèfon <b>'.$tel.'</b>.<br /><br />Disculpi les molèsties</div>'; break;
                                             case 'ABANS_PERIODE_MATRICULA_AA_IDIOMES': echo '<div>Vostè podrà matricular-se a aquest curs per internet a partir del dia '.date('d/m/Y',$dataiA).' si vol continuar els estudis d\'idiomes.<br /><br /> Per a més informació pot posar-se en contacte amb <b>'.$nom.'</b> enviant un correu electrònic a <b>'.$email.'</b> o bé trucant al <b>'.$tel.'</b></div>'; break;
-                                            case 'ABANS_PERIODE_MATRICULA': echo '<div>Vostè podrà matricular-se a aquest curs per internet a partir del dia '.date('d/m/Y',$datai).'.<br /><br /> Per a més informació pot posar-se en contacte amb <b>'.$nom.'</b> enviant un correu electrònic a <b>'.$email.'</b> o bé trucant al <b>'.$tel.'</b></div>'; break;
+                                            case 'ABANS_PERIODE_MATRICULA': echo '<div>Vostè podrà matricular-se a aquest curs per internet a partir del dia '.date( 'd/m/Y' , $CURS->getDatainmatricula() ).'.<br /><br /> Per a més informació pot posar-se en contacte amb <b>'.$nom.'</b> enviant un correu electrònic a <b>'.$email.'</b> o bé trucant al <b>'.$tel.'</b></div>'; break;
                                             case 'POT_MATRICULAR':  echo mostraFormulari( $OS->getNom() , $CURS , $MISSATGE , $ESTAT ); break;       
                                             case 'LLISTA_ESPERA':   echo mostraFormulari( $OS->getNom() , $CURS , $MISSATGE , $ESTAT ); break;                                            
                                         }                                                                        
