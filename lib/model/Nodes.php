c87cb2f27ca2cc4a4c6691cbf95810fac1371bc7
Albert Johé <albert.johe@gmail.com>
2012-12-28 10:13:52 +0000

    ENH: Afegida una funció que torna l'arbre de menus a partir d'un menú.
    git-svn-id: https://servidor.casadecultura.cat/public/ccg/trunk/intranet@901 6722b150-0e3f-45da-bd85-83856719fb75

--- lib/model/Nodes.php
+++ lib/model/Nodes.php
@@ -22,4 +22,21 @@ class Nodes extends BaseNodes
     return myUser::text2url($nom);
   }
   
+  /**
+   * Retorna l'arbre de nodes del directori
+   **/
+  public function getArbre(){
+    
+    //Variable per assegurar uqe no hi hagi cap error en l'arbre.
+    $i = 0;
+    
+    $RET = array( $this->getIdnodes() => $this );    
+    $NODE_ACTUAL = NodesPeer::retrieveByPK( $this->getIdpare() );
+    while( ! is_null( $NODE_ACTUAL ) && $i++ < 10 ){
+        $RET[ $NODE_ACTUAL->getIdnodes() ] = $NODE_ACTUAL;
+        $NODE_ACTUAL = NodesPeer::retrieveByPK( $NODE_ACTUAL->getIdpare() );           
+    }
+    return $RET;
+  }
+  
 }
